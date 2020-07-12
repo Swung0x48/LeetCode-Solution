40a43f9d13e0a65efc51a8c78671cff68a685ef8
Swung0x48 <swung0x48@outlook.com>
2020-07-12 21:08:33 +0800

    AC: 347

--- 347/Solution.java
+++ 347/Solution.java
@@ -1,52 +1,31 @@
 import java.util.*;
 
-class MapUtil {
-    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue( Map<K, V> map ){
-        ValueComparator<K,V> bvc =  new ValueComparator<K,V>(map);
-        TreeMap<K,V> sorted_map = new TreeMap<K,V>(bvc);
-        sorted_map.putAll(map);
-        return sorted_map;
-    }
-
-}
-
-class ValueComparator<K, V extends Comparable<? super V>> implements Comparator<K> {
-    Map<K, V> base;
-    public ValueComparator(Map<K, V> base) {
-        this.base = base;
-    }
-
-    public int compare(K a, K b) {
-        int result = (base.get(a).compareTo(base.get(b)));
-        if (result == 0) result=1;
-        // returning 0 would merge keys
-        return result;
-    }
-}
-
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        if (nums.length == k) return nums;
+        
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i: nums)
         {
-            try
-            {
-                map.put(i, map.get(i) + 1);
-            }
-            catch (NullPointerException e)
-            {
-                map.put(i, 1);
-            }
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
-        MapUtil.sortByValue(map);
 
-        List<Integer> ret = new ArrayList<Integer>();
-        Integer[] arr = map.keySet().toArray(new Integer[0]);
-        for (int i = 0; i != k; i ++)
+
+        Queue<Integer> heap = new PriorityQueue<Integer>(
+            (n1, n2) -> map.get(n1) - map.get(n2));
+        
+        for (int i: map.keySet())
         {
-            ret.add(arr[i]);
+            heap.add(i);
+            if (heap.size() > k) heap.poll();
         }
-
-        return ret.stream().mapToInt(item -> item).toArray();
+        
+        int[] ret = new int[k];
+        for (int i = 0; i < k; i ++)
+        {
+            ret[i] = heap.poll();
+        }
+        
+        return ret;
     }
 }
