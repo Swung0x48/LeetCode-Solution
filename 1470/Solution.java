748bb0dc8140c597428c98498e7baf30357fc638
Swung0x48 <swung0x48@outlook.com>
2020-09-09 20:48:27 +0800

    AC: 1470 (5.86% / 33.14%)

---
+++ 1470/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+        {
+            list.add(nums[i]);
+            list.add(nums[n + i]);
+        }
+
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
