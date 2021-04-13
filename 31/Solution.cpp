d1866592eca57219757170f599746469354471e4
Swung0x48 <swung0x48@outlook.com>
2021-04-13 23:47:47 +0800

    AC: 31 (76.33% / 64.73%)

---
+++ 31/Solution.cpp
@@ -0,0 +1,9 @@
+#include <algorithm>
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if (!std::next_permutation(nums.begin(), nums.end()))
+            std::sort(nums.begin(), nums.end());
+    }
+};
